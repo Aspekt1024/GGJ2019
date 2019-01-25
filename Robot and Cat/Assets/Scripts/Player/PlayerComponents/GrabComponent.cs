f6f2a6f77fe66185ff3071debfb361f604d66e35
Holiday Course <holiday@ad.aie.edu>
2019-01-26 02:58:02 +1100

    grabbable object start

---
+++ Robot and Cat/Assets/Scripts/Player/PlayerComponents/GrabComponent.cs
@@ -0,0 +1,42 @@
+﻿using RobotCat.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace RobotCat.Player
+{
+    public class GrabComponent : MonoBehaviour
+    {
+        public Transform ObjectHoldPositioner;
+
+        private GrabbableObject currentHeld;
+        private GameObject currentFocus;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                CheckforObject();   
+            }   
+        }
+
+        private void CheckforObject()
+        {
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            bool hitObject = Physics.Raycast(ray, out hit, 3f);
+
+            if (hitObject && hit.collider.gameObject.layer == LayerMask.NameToLayer("GrabbableObject"))
+            {
+                currentHeld = hit.collider.gameObject.GetComponentInParent<GrabbableObject>();
+                if (currentHeld != null)
+                {
+                    currentHeld.transform.position = ObjectHoldPositioner.position;
+                }
+            }
+        }
+    }
+}
