430856ffa090087c3cf06b062b37c5f6a67ffcb9
Holiday Course <holiday@ad.aie.edu>
2019-01-26 00:16:40 +1100

    Updated to C#4

---
+++ Robot and Cat/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RobotCat
+{
+    public class GameManager : MonoBehaviour
+    {
+        private RCStatics statics;
+
+
+        void Awake()
+        {
+            statics = new RCStatics(this);
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
+
+
---
+++ Robot and Cat/Assets/Scripts/Managers/RCStatics.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace RobotCat
+{
+    public class RCStatics
+    {
+        private static RCStatics instance;
+
+        private GameManager gameManager;
+
+        public RCStatics(GameManager gameManager)
+        {
+            if (instance != null)
+            {
+                Debug.LogError($"Detected multiple versions of {nameof(RCStatics)}");
+            }
+
+            instance = this;
+        }
+
+
+
+    }
+}
