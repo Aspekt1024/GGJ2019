a6c38e4347c882eed4149e36ac95281800990c9f
Aspekt1024 <d.szabo87@gmail.com>
2019-01-27 01:47:14 +1100

    shift to run!

--- Robot and Cat/Assets/Scripts/Player/PlayerComponents/MovementComponent.cs
+++ Robot and Cat/Assets/Scripts/Player/PlayerComponents/MovementComponent.cs
@@ -12,11 +12,13 @@ namespace RobotCat.Player
 
         private Rigidbody body;
         public AnimationCurve movementCurve;
-        public float maxSpeed = 5.0f;
+        public float maxSpeed = 3.0f;
+        public float maxRunSpeed = 5f;
         public float accelerationTime = 0.0f;
         public float timeToMaxSpeed = 1.0f;
         private float timeSinceInput = 0.0f;
         private bool keyDown = false;
+
 
         private void Awake()
         {
@@ -46,13 +48,15 @@ namespace RobotCat.Player
             Vector3 strafeVel = body.transform.right;
             float resolvedSpeed = 0.0f;
 
+            float speed = (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) ? maxRunSpeed : maxSpeed;
+
             if ((Mathf.Abs(translation) > 0.01) || (Mathf.Abs(strafe) > 0.01))
             {
                 timeSinceInput += Time.deltaTime/timeToMaxSpeed;
                 timeSinceInput = Mathf.Clamp(timeSinceInput, 0.0f, 1.0f);
 
                 accelerationTime = movementCurve.Evaluate(timeSinceInput);
-                resolvedSpeed = accelerationTime * maxSpeed;
+                resolvedSpeed = accelerationTime * speed;
 
                 forwardVel = body.transform.forward * translation;
                 strafeVel = body.transform.right * strafe;
@@ -64,7 +68,7 @@ namespace RobotCat.Player
                 timeSinceInput = Mathf.Clamp(timeSinceInput, 0.0f, 1.0f);
 
                 accelerationTime = movementCurve.Evaluate(timeSinceInput);
-                resolvedSpeed = accelerationTime * maxSpeed;
+                resolvedSpeed = accelerationTime * speed;
                 forwardVel = body.velocity;
                 strafeVel = new Vector3(0f, 0f, 0f) ;
 
